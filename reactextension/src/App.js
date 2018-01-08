665f3ff31578fb43a9cfff6e7479682be5b69756
dtthor <d.t.thorwaldson@gmail.com>
2018-01-08 11:08:57 -0600

    renders component skeletons in App.js

--- reactextension/src/App.js
+++ reactextension/src/App.js
@@ -16,14 +16,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">FriendKeeper</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <Landing />
+        <Add />
+        <Edit />
       </div>
     );
   }
