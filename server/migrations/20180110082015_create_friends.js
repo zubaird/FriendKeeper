44f9845efb91bf8acd3c78b933d94380186b40d8
dtthor <d.t.thorwaldson@gmail.com>
2018-01-19 14:37:26 -0600

    adds edit friend form

--- server/migrations/20180110082015_create_friends.js
+++ server/migrations/20180110082015_create_friends.js
@@ -3,14 +3,14 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('friends', (table)=>{
     table.increments();
     table.string('name').notNullable();
-    table.string('phone_number').notNullable();
+    table.string('phone_number');
     table.string('email');
-    table.date('birthday');
-    table.date('anniversary');
+    table.string('birthday').notNullable();
+    table.string('anniversary');
     table.integer('date_added');
-    table.integer('email_interval');
-    table.integer('phone_interval');
-    table.integer('text_interval');
+    table.integer('email_interval').notNullable();
+    table.integer('phone_interval').notNullable();
+    table.integer('text_interval').notNullable();
   })
 };
 
--- server/seeds/01friends.js
+++ server/seeds/01friends.js
@@ -5,16 +5,16 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('friends').insert([
-        {name: "Josh Currie", phone_number: '(925) 286-9709', email: 'josh@currie.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 90, phone_interval: 14, text_interval: 7},
-        {name: "Kevin Adams", phone_number: '(281) 330-8004', email: 'kevin@adams.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 60, phone_interval: 21, text_interval: 5},
-        {name: "Stanton Morrow", phone_number: '(214) 263-9709', email: 'stanton@morrow.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 30, phone_interval: 30, text_interval: 10},
-        {name: "Cameron Alspaw", phone_number: '(469) 369-2230', email: 'cameron@al.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 40, phone_interval: 14, text_interval: 3},
-        {name: "Jason Jones", phone_number: '(310) 283-9912', email: 'jason@jones.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 50, phone_interval: 5, text_interval: 2},
-        {name: "Adam Hundley", phone_number: '(832) 586-7459', email: 'adam@hundley.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 60, phone_interval: 10, text_interval: 2},
-        {name: "Steve Rizzotto", phone_number: '(281) 416-9341', email: 'steve@rizzotto.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 70, phone_interval: 30, text_interval: 10},
-        {name: "Dane Thorwaldson", phone_number: '(832) 622-9709', email: 'dane@thorwaldson.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 80, phone_interval: 14, text_interval: 5},
-        {name: "Reid Curry", phone_number: '(713) 286-5214', email: 'reid@curry.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 90, phone_interval: 25, text_interval: 9},
-        {name: "Josh Marx", phone_number: '(214) 190-1153', email: 'josh@marx.com', birthday: null, anniversary: null, date_added: Math.floor(Date.now()/100000), email_interval: 100, phone_interval: 45, text_interval: 3}
+        {name: "Josh Currie", phone_number: '(925) 286-9709', email: 'josh@currie.com', birthday: '12/19/1989', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 90, phone_interval: 14, text_interval: 7},
+        {name: "Kevin Adams", phone_number: '(281) 330-8004', email: 'kevin@adams.com', birthday: '05/21/1990', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 60, phone_interval: 21, text_interval: 5},
+        {name: "Stanton Morrow", phone_number: '(214) 263-9709', email: 'stanton@morrow.com', birthday: '12/19/1989', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 30, phone_interval: 30, text_interval: 10},
+        {name: "Cameron Alspaw", phone_number: '(469) 369-2230', email: 'cameron@al.com', birthday: '02/08/1990', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 40, phone_interval: 14, text_interval: 3},
+        {name: "Jason Jones", phone_number: '(310) 283-9912', email: 'jason@jones.com', birthday: '12/19/1999', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 50, phone_interval: 5, text_interval: 2},
+        {name: "Adam Hundley", phone_number: '(832) 586-7459', email: 'adam@hundley.com', birthday: '12/19/1980', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 60, phone_interval: 10, text_interval: 2},
+        {name: "Steve Rizzotto", phone_number: '(281) 416-9341', email: 'steve@rizzotto.com', birthday: '01/05/1989', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 70, phone_interval: 30, text_interval: 10},
+        {name: "Dane Thorwaldson", phone_number: '(832) 622-9709', email: 'dane@thorwaldson.com', birthday: '09/19/1985', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 80, phone_interval: 14, text_interval: 5},
+        {name: "Reid Curry", phone_number: '(713) 286-5214', email: 'reid@curry.com', birthday: '09/22/1989', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 90, phone_interval: 25, text_interval: 9},
+        {name: "Josh Marx", phone_number: '(214) 190-1153', email: 'josh@marx.com', birthday: '12/19/1989', anniversary: '02/08/1990', date_added: Math.floor(Date.now()/100000), email_interval: 100, phone_interval: 45, text_interval: 3}
       ]);
     });
 };
