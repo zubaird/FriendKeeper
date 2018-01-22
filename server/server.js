20ee06b3df4c8da9cbe5601dc4948a0aff6b33df
dtthor <d.t.thorwaldson@gmail.com>
2018-01-21 21:02:03 -0600

    changes port to 3000

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const PORT = process.env.NODE_ENV || 8080;
+const PORT = process.env.NODE_ENV || 3000;
 
 
 
