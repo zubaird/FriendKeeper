a50b0ce3ddedcbc0d0c3d731778d12088cf43d11
dtthor <d.t.thorwaldson@gmail.com>
2018-01-21 21:55:14 -0600

    changes extension popup

--- popup.js
+++ popup.js
@@ -4,5 +4,5 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function goToLanding() {
- chrome.tabs.create({active: true, url: "localhost:8080"});
+ chrome.tabs.create({active: true, url: "localhost:3000"});
 }
